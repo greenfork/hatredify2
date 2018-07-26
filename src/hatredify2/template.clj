7f6de03f4bedcc30e87ed3b8d0e2f3e9c433dc0f
Dmitry Matveyev <dm.matveyev@protonmail.com>
2018-07-26 21:18:04 +0300

    Add html post-processing of final strings

--- src/hatredify2/template.clj
+++ src/hatredify2/template.clj
@@ -1,5 +1,6 @@
 (ns hatredify2.template
-  (:require [net.cgrand.enlive-html :as html]))
+  (:require [net.cgrand.enlive-html :as html]
+            [hatredify2.core :refer [identifier]]))
 
 (html/deftemplate index "templates/index.html"
   [{:keys [pure-chunk hatredified-chunk request-method]}]
@@ -7,3 +8,17 @@
   [:textarea#pure-chunk] (html/content pure-chunk)
   [:textarea#hatredified-chunk] (when (= request-method :post)
                                   (html/content hatredified-chunk)))
+
+(def highlight-token "<span class=\"highlight\">")
+
+(defn html-highlight-word
+  "Use html tags to highlight a `word`."
+  [word]
+  (str highlight-token word "</span"))
+
+(defn highlight
+  "Highlight all words written with capitals and with `identifier` at the end.
+  Remove any `identifier`."
+  [text]
+  (let [re (re-pattern (str "\\b([A-Z]{2,})" identifier "\\b"))]
+    (clojure.string/replace text re (html-highlight-word "$1"))))
